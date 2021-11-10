72952675504f1e54937a4760ef17f4c12ba53117
shifair <farisnura@gmail.com>
2021-11-10 08:16:28 +0700

    Fixing again put optional chaining

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,7 +1,8 @@
 import { appConstants } from "../constants";
 
 export const initialState = {
-  products: []
+  products: [],
+  duplicatedArrKey: [],
 };
 
 const re = /([A-Z])\w*/g;
@@ -9,7 +10,7 @@ const re = /([A-Z])\w*/g;
 const appReducer = (state = initialState, action) => {
   // * Filtered Only Product Code
   const filteredProductCode = action?.payload?.map(
-    (item) => item?.productCode?.match(re)?.[0]
+    (item) => item.productCode?.match(re)?.[0]
   );
 
   //* Removed Duplicate Values in Filtered Only Product Code Array
@@ -18,21 +19,19 @@ const appReducer = (state = initialState, action) => {
   //* Grouping Array By Product Code
   const filteredArr = [
     action?.payload?.reduce(function (r, a) {
-      r[a.productCode.match(re)[0]] = r[a.productCode.match(re)?.[0]] || [];
-      r[a.productCode.match(re)[0]].push(a);
+      r[a?.productCode?.match(re)?.[0]] = r[a?.productCode?.match(re)?.[0]] || [];
+      r[a?.productCode?.match(re)?.[0]].push(a);
       return r;
-    }, Object.create(null))
+    }, Object.create(null)),
   ];
 
   //* Merged Array Into a Whole New List Array
   const newArr = removedDuplicatedArr?.map((firstItem) => ({
-    listProducts: filteredArr[0][firstItem]
+    listProducts: filteredArr[0][firstItem],
   }));
 
   // * Filtering Array with List of Sorted Images
-  const filterSortedArr = newArr.map((firstItem) =>
-    firstItem.listProducts.map((secondItem) => secondItem.imageFile)
-  );
+  const filterSortedArr = newArr.map((firstItem) => firstItem.listProducts.map((secondItem) => secondItem.imageFile))
 
   // * Fixed New Array Products
   const fixedNewArr = filterSortedArr.map((firstItem, index) => ({
@@ -40,15 +39,16 @@ const appReducer = (state = initialState, action) => {
     contentSlot: "",
     productName: "",
     productCategory: null,
-    productImages: firstItem
+    productImages: firstItem,
   }));
 
   switch (action.type) {
     case appConstants.UPLOAD_PRODUCT_SUCCESS:
       return {
         ...state,
-        products: fixedNewArr
+        products: fixedNewArr,
       };
+
     default:
       return state;
   }
